f68b73048e2fec74587b0e304606174e7e12f144
Milena <milenarodriguesfulfaro@aluno.itj.g12.br>
2026-03-19 12:42:58 -0300

    DML do projeto
    DML para definição e testes acertivos sobre as capacidades do banco, prova de que tudo funciona
    (EXECUTE DEPOIS DO DDL)

---
+++ bancoPojetoDML.sql
@@ -0,0 +1,13 @@
+INSERT INTO jogador (nickname, senha) VALUES ('Rodrigo', 'Cookies29');
+
+INSERT INTO personagem (nome,vida,defesa,forca,velocidade,alcance)
+VALUES ('Maguinho', 100,50,100,120,150);
+
+INSERT INTO item (nome,tamanho,dano)
+VALUES ('Poção', 0.50, 0);
+
+INSERT INTO item (nome,tamanho,dano)
+VALUES ('Varinha', 0.80, 50);
+
+INSERT INTO item (nome,tamanho,dano)
+VALUES ('Chapéu de mago', 0.60, 10)
